8a94bcac4d98b819d9bd7a9c7eb21f85f84d8618
FarzanaPrianka <farzana.prianka@gmail.com>
2020-03-09 16:45:38 -0400

    Updates

---
+++ precache-manifest.155a3e9a31f944be71abb5996f03e8ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e381b76f2399ddf52857ac60ccd31253",
+    "url": "/haikus-highway/index.html"
+  },
+  {
+    "revision": "afdc083b261cc6b5db33",
+    "url": "/haikus-highway/static/css/main.2efd75db.chunk.css"
+  },
+  {
+    "revision": "fe3d15082ff8c0bca7d1",
+    "url": "/haikus-highway/static/js/2.82e7dfb6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/haikus-highway/static/js/2.82e7dfb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afdc083b261cc6b5db33",
+    "url": "/haikus-highway/static/js/main.ca949c32.chunk.js"
+  },
+  {
+    "revision": "4ca0230651b243dc008e",
+    "url": "/haikus-highway/static/js/runtime-main.7e66e215.js"
+  }
+]);
